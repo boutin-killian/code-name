011b8d5c31decfea552c5e00eee102ac3f837af5
Killian BOUTIN <killian.boutin@gosselink.fr>
2020-02-11 11:55:35 +0100

    [U] Update Cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, {useEffect, useState, createContext} from 'react';
-import './App.css';
-
+import React, { useState, createContext, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Container, Menu, Icon } from "semantic-ui-react";
-
+import "./App.css";
+import BookList from "./components/BookList";
 import CartSummary from "./components/CartSummary";
 import CartDetails from "./components/CartDetails";
 
@@ -11,93 +10,92 @@ export const CartContext = createContext();
 const CART_KEY = "react-shop";
 
 function App() {
+  const [cart, setCart] = useState({});
+  const [nbArticles, setNbArticles] = useState(0);
 
-    const [cart, setCart] = useState({});
-    const [nbArticles, setNbArticles] = useState(0);
-
-    useEffect(() => {
-        const cartFromStorage = localStorage.getItem(CART_KEY);
-        if (cartFromStorage !== null) {
-            setCart(JSON.parse(cartFromStorage));
-        }
-    }, []);
-
-    useEffect(() => {
-        // only strings in localStorage
-        localStorage.setItem(CART_KEY, JSON.stringify(cart));
-        document.title = `caddie(${nbArticles})`;
-    }, [cart, nbArticles]);
-
-    function addToCart(item) {
-        console.log("item", item);
-        if (!cart[item.id]) {
-            cart[item.id] = item;
-            cart[item.id].quantity = 1;
-        } else {
-            cart[item.id].quantity += 1;
-        }
-        setCart({ ...cart });
-        console.log("cart", cart);
+  //!\ order matters: first useEffect() retrieves from localStorage, second useEffect persists in localStorage
+  useEffect(() => {
+    const cartFromStorage = localStorage.getItem(CART_KEY);
+    if (cartFromStorage !== null) {
+      setCart(JSON.parse(cartFromStorage));
     }
+  }, []);
 
-    function removeFromCart(item) {
-        if (cart[item.id].quantity !== 1) {
-            cart[item.id].quantity = cart[item.id].quantity - 1;
-        } else {
-            delete cart[item.id];
-        }
-        setCart({ ...cart });
-        console.log("cart", cart);
+  useEffect(() => {
+    // only strings in localStorage
+    localStorage.setItem(CART_KEY, JSON.stringify(cart));
+    document.title = `caddie(${nbArticles})`;
+  }, [cart, nbArticles]);
+
+  function addToCart(item) {
+    console.log("item", item);
+    if (!cart[item.id]) {
+      cart[item.id] = item;
+      cart[item.id].quantity = 1;
+    } else {
+      cart[item.id].quantity += 1;
     }
+    setCart({ ...cart });
+    console.log("cart", cart);
+  }
 
-    function emptyCart() {
-        console.log('ici');
-        const response = window.confirm(
-            "Etes-vous vous sûr de vouloir vider le caddie ? "
-        );
-        if (response) {
-            setCart({});
-        }
+  function removeFromCart(item) {
+    if (cart[item.id].quantity !== 1) {
+      cart[item.id].quantity = cart[item.id].quantity - 1;
+    } else {
+      delete cart[item.id];
     }
+    setCart({ ...cart });
+    console.log("cart", cart);
+  }
 
-    function countCartArticles() {
-        let total = 0;
-        Object.keys(cart).map(key => (total += cart[key].quantity));
-        setNbArticles(total);
-        return total;
+  function emptyCart() {
+    const response = window.confirm(
+      "Etes-vous vous sûr de vouloir vider le caddie ? "
+    );
+    if (response) {
+      setCart({});
     }
+  }
 
-    const contextValue = {
-        cart,
-        addToCart,
-        countCartArticles,
-        removeFromCart,
-        emptyCart
-    };
+  function countCartArticles() {
+    let total = 0;
+    Object.keys(cart).map(key => (total += cart[key].quantity));
+    setNbArticles(total);
+    return total;
+  }
 
-    return (
-      <>
-          <Router>
-              <CartContext.Provider value={contextValue}>
-                  <Container>
-                      <Menu stackable>
-                          <Menu.Item>
-                              <Link to="/">Campus Shop</Link>
-                          </Menu.Item>
-                          <Menu.Item>
-                              <Link to="/cart">
-                                  <Icon name="cart" size="small" /> <CartSummary />
-                              </Link>
-                          </Menu.Item>
-                      </Menu>
-                  </Container>
-                  <Switch>
-                      <Route path="/cart" component={CartDetails} />
-                      <Route path="/" />
-                  </Switch>
-              </CartContext.Provider>
-          </Router>
-      </>
+  const contextValue = {
+    cart,
+    addToCart,
+    countCartArticles,
+    removeFromCart,
+    emptyCart
+  };
+
+  return (
+    <>
+      <Router>
+        <CartContext.Provider value={contextValue}>
+          <Container>
+            <Menu stackable>
+              <Menu.Item>
+                <Link to="/">Lwar</Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to="/cart">
+                  <Icon name="cart" size="small" /> <CartSummary />
+                </Link>
+              </Menu.Item>
+            </Menu>
+          </Container>
+          <Switch>
+            <Route path="/cart" component={CartDetails} />
+            <Route path="/" component={BookList} />
+          </Switch>
+        </CartContext.Provider>
+      </Router>
+    </>
   );
 }
 
