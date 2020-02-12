67b887542b76deaa764ca085e7bda194fa3e0b18
Geoffrey <gef56150@gmail.com>
2020-02-12 14:48:38 +0100

    Change AddArticle function to component

--- src/components/AddArticle.js
+++ src/components/AddArticle.js
@@ -1,21 +1,18 @@
-import React ,{ useState } from 'react';
+import React from 'react';
 import { Button, Form } from 'semantic-ui-react'
 import axios from "axios";
 
-export default function AddArticle({ props, user }) {
-  constructor(props) {
-    super(props)
-		this.state = {
+export default class AddArticle extends React.Component {
+  	state = {
         title: '',
         year: '',
         price: '',
         type: '',
         image: '',
-        user: user, 
+        user: this.props.user, 
       };
-	}
 
-  const onChange = (e) => {
+  onChange = (e) => {
     this.setState({ title: e.target.value });
     this.setState({ year: e.target.value });
     this.setState({ price: e.target.value });
@@ -23,7 +20,7 @@ export default function AddArticle({ props, user }) {
     this.setState({ image: e.target.value });
   }
 
-  const handleSubmit = article => {
+  handleSubmit = article => {
     console.log('article', article);
     const config = {
       'Content-Type': 'application/json'
@@ -36,35 +33,38 @@ export default function AddArticle({ props, user }) {
       .catch(err => console.error(err));
   };
 
-  return (
-    <>
-      Ajouter un article
-      <Form onSubmit={handleSubmit}>
-        <Form.Field>
-          <label>Title</label>
-          <input placeholder='title' onChange={onChange} />
-        </Form.Field>
-        <Form.Field>
-          <label>Year</label>
-          <input placeholder='year' type='date' onChange={onChange} />
-        </Form.Field>
-        <Form.Field>
-          <label>Price</label>
-          <input placeholder='price' type='number' onChange={onChange} />
-        </Form.Field>
-        <Form.Field label='An HTML <select>' control='select' onChange={onChange}>
-          <option value='male'>Book</option>
-          <option value='female'>Music</option>
-          <option value='female'>Photo</option>
-          <option value='female'>Video</option>
-        </Form.Field>
-        <Form.Field>
-          <label>Image</label>
-          <input placeholder='url' onChange={onChange} />
-        </Form.Field>
-
-        <Button type='submit'>Submit</Button>
-      </Form>
-    </>
-  );
+  render() {
+  
+    return (
+      <>
+        Ajouter un Article
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label>Title</label>
+            <input placeholder='title' onChange={this.onChange} />
+          </Form.Field>
+          <Form.Field>
+            <label>Year</label>
+            <input placeholder='year' type='date' onChange={this.onChange} />
+          </Form.Field>
+          <Form.Field>
+            <label>Price</label>
+            <input placeholder='price' type='number' onChange={this.onChange} />
+          </Form.Field>
+          <Form.Field label='An HTML <select>' control='select' onChange={this.onChange}>
+            <option value='male'>Book</option>
+            <option value='female'>Music</option>
+            <option value='female'>Photo</option>
+            <option value='female'>Video</option>
+          </Form.Field>
+          <Form.Field>
+            <label>Image</label>
+            <input placeholder='url' onChange={this.onChange} />
+          </Form.Field>
+    
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </>
+    );
+  }
 }
